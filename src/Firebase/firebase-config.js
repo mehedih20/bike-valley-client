7b6f03074d8f16335b1cc903e751bdcf844bdd54
mehedih20 <mehedih20@gmail.com>
2023-07-19 08:18:12 +0530

    minor tweak

--- src/Firebase/firebase-config.js
+++ src/Firebase/firebase-config.js
@@ -1,19 +1,10 @@
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBeUY9XfCjwD1yZfzRrIrQNA-u_Ahcc3xM",
-  authDomain: "my-bike-valley.firebaseapp.com",
-  projectId: "my-bike-valley",
-  storageBucket: "my-bike-valley.appspot.com",
-  messagingSenderId: "628483133853",
-  appId: "1:628483133853:web:aa0dcaf447cfb55d6d1f74",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebaseConfig;
